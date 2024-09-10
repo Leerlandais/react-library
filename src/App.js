28e546ec88a07d95680e5ce46328d83a9af533d1
leerlandais <leerlandais@gmail.com>
2024-09-10 11:51:03 +0100

    schooltime

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from "./components/Heading";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Heading level={6} text={"Hello There"} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
